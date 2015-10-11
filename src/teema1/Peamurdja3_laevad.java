66ba6d8793c845aab0bec81fb371f64d30cd22f4
Aire Rihe <arihe@klass1-05-l.intra.itcollege.ee>
2015-10-11 12:27:04 +0300

    laevade pommitamine

--- src/teema1/Peamurdja3_laevad.java
+++ src/teema1/Peamurdja3_laevad.java
@@ -15,5 +15,49 @@ import java.util.Scanner;
  * 3. Kas oskad ka nii, et laevad ei ole 1x1 vaid 1x2 ja 1x3 ja orienteeruvad
  *    nii vertikaalselt kui ka horisontaalselt?
  */
+
+
+
 public class Peamurdja3_laevad {
+
+    public static void main(String[] args) {
+        int[] laud = new int[8];
+        int i;
+        for (i = 0; i < (laud.length - 1); i++) {
+            laud[i] = (int) (Math.random() * 2);
+        }
+        System.out.println(Arrays.toString(laud));
+        Scanner kasutaja = new Scanner(System.in);
+        for (i = 0; i < (laud.length - 1); i++) {
+            if (laud[i] == 1) {
+                System.out.println("Sisesta number ühest kaheksani");
+                int sisestus = kasutaja.nextInt() - 1;
+                if (laud[sisestus] == 1) {
+                    System.out.println("Pihtas!");
+                    laud[sisestus] = 0;
+                }
+                else {
+                    System.out.println("Mööda!");
+                }
+
+            }
+
+            /* int Math.random(laud[1]) * 2;
+            System.out.println(laud [4]);
+            public static int laev() {
+            int tulemus = (int) (Math.random() * 2);
+            return tulemus;
+            int[] laud = {laev(), laud(), laud(), laud(), laud(), laud(), laud()}
+            while (true) {}
+            for (int i = 0; i < laud.Length; i++) {}
+            sout(laud.Length)
+            */
+
+        }
+        System.out.println("Mäng on läbi!");
+    }
 }
+
+/**
+
+ */
